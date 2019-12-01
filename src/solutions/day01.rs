12448ff7286e018bd6f0bdb15e7583a1a3e13522
Marc Noirot <marc.noirot@gmail.com>
2019-12-01 22:05:16 +0100

    Improve code

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,6 +1,8 @@
 use crate::solver::Solver;
-use std::io::{self, BufRead, BufReader};
-use std::iter::successors;
+use std::{
+    io::{self, BufRead, BufReader},
+    iter::successors,
+};
 
 pub struct Problem;
 
@@ -15,7 +17,7 @@ impl Solver for Problem {
     }
 
     fn solve_first(&self, input: &Vec<u64>) -> u64 {
-        input.iter().cloned().map(mass_fuel).sum()
+        input.iter().cloned().map(module_fuel).sum()
     }
 
     fn solve_second(&self, input: &Vec<u64>) -> u64 {
@@ -23,12 +25,12 @@ impl Solver for Problem {
     }
 }
 
-fn mass_fuel(mass: u64) -> u64 {
+fn module_fuel(mass: u64) -> u64 {
     (mass / 3).saturating_sub(2)
 }
 
 fn total_fuel_mass(mass: u64) -> u64 {
-    successors(Some(mass_fuel(mass)), |&m| Some(mass_fuel(m)))
+    successors(Some(module_fuel(mass)), |&m| Some(module_fuel(m)))
         .take_while(|&m| m != 0)
         .sum()
 }
@@ -38,7 +40,7 @@ fn total_fuel_mass_orig(mass: u64) -> u64 {
     let mut total = 0u64;
     let mut m = mass;
     while m != 0 {
-        m = mass_fuel(m);
+        m = module_fuel(m);
         total += m;
     }
     total
@@ -49,16 +51,16 @@ mod tests {
     use crate::solutions::day01::*;
 
     #[test]
-    fn test_mass_fuel() {
-        assert_eq!(mass_fuel(12), 2);
-        assert_eq!(mass_fuel(14), 2);
-        assert_eq!(mass_fuel(1969), 654);
-        assert_eq!(mass_fuel(100_756), 33583);
-        assert_eq!(mass_fuel(1), 0);
+    fn test_module_fuel() {
+        assert_eq!(module_fuel(12), 2);
+        assert_eq!(module_fuel(14), 2);
+        assert_eq!(module_fuel(1969), 654);
+        assert_eq!(module_fuel(100_756), 33583);
+        assert_eq!(module_fuel(1), 0);
     }
 
     #[test]
-    fn test_total_mass_fuel() {
+    fn test_total_fuel_mass() {
         assert_eq!(total_fuel_mass(14), 2);
         assert_eq!(total_fuel_mass(1969), 966);
         assert_eq!(total_fuel_mass(100_756), 50346);
