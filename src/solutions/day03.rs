28f9f711d010c2debd551db7f8e43df99e0b0d84
Marc Noirot <marc.noirot@gmail.com>
2019-12-05 00:05:07 +0100

    Improve solution for day 03

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -10,28 +10,27 @@ use std::{
 pub struct Problem;
 
 impl Solver for Problem {
-    type Input = (Vec<Instruction>, Vec<Instruction>);
+    type Input = (Vec<Point>, Vec<Point>);
     type Output1 = u64;
     type Output2 = u64;
 
-    fn parse_input<R: Read>(&self, r: R) -> (Vec<Instruction>, Vec<Instruction>) {
+    fn parse_input<R: Read>(&self, r: R) -> Self::Input {
         let mut lines = BufReader::new(r).lines().flatten();
 
         let l = lines.next().expect("error reading first line");
-        let v1 = instructions_from_str(&l);
+        let v1 = str_to_instructions(&l);
+        let v1 = instructions_to_points(&v1);
 
         let l = lines.next().expect("error reading second line");
-        let v2 = instructions_from_str(&l);
+        let v2 = str_to_instructions(&l);
+        let v2 = instructions_to_points(&v2);
 
         (v1, v2)
     }
 
-    fn solve_first(&self, (i1, i2): &(Vec<Instruction>, Vec<Instruction>)) -> u64 {
-        let p1 = points_from_instructions(&i1);
-        let p2 = points_from_instructions(&i2);
-
-        let p1_set: HashSet<Point> = HashSet::from_iter(p1.into_iter());
-        let p2_set: HashSet<Point> = HashSet::from_iter(p2.into_iter());
+    fn solve_first(&self, (p1, p2): &Self::Input) -> Self::Output1 {
+        let p1_set: HashSet<Point> = HashSet::from_iter(p1.clone().into_iter());
+        let p2_set: HashSet<Point> = HashSet::from_iter(p2.clone().into_iter());
 
         p1_set
             .intersection(&p2_set)
@@ -40,12 +39,9 @@ impl Solver for Problem {
             .unwrap_or(0)
     }
 
-    fn solve_second(&self, (i1, i2): &(Vec<Instruction>, Vec<Instruction>)) -> u64 {
-        let p1 = points_from_instructions(&i1);
-        let p2 = points_from_instructions(&i2);
-
-        let p1_set: HashSet<Point> = HashSet::from_iter(p1.into_iter());
-        let p2_set: HashSet<Point> = HashSet::from_iter(p2.into_iter());
+    fn solve_second(&self, (p1, p2): &Self::Input) -> Self::Output2 {
+        let p1_set: HashSet<Point> = HashSet::from_iter(p1.clone().into_iter());
+        let p2_set: HashSet<Point> = HashSet::from_iter(p2.clone().into_iter());
 
         let inter = p1_set.intersection(&p2_set).collect::<Vec<_>>();
 
@@ -62,17 +58,16 @@ impl Solver for Problem {
     }
 }
 
-fn instructions_from_str(s: &str) -> Vec<Instruction> {
-    s.trim()
-        .split(',')
+fn str_to_instructions(s: &str) -> Vec<Instruction> {
+    s.split(',')
         .map(|c| Instruction {
-            dir: c[0..1].parse::<Dir>().expect("error parsing "),
+            dir: c[0..1].parse::<Dir>().expect("error parsing dir"),
             size: c[1..].parse::<isize>().expect("error parsing size"),
         })
         .collect::<Vec<_>>()
 }
 
-fn points_from_instructions(instructions: &[Instruction]) -> Vec<Point> {
+fn instructions_to_points(instructions: &[Instruction]) -> Vec<Point> {
     let mut v = vec![];
     let mut pt = Point {
         x: 0,
